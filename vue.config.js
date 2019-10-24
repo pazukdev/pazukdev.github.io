1ec5c45548e945c3236da0b8c32e84568cfaffa2
pazukdev <pazukdev@gmail.com>
2019-10-24 18:36:28 +0300

    for gh pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     // options...
     devServer: {
-        proxy: 'https://bearings-info.herokuapp.com/',
+        // proxy: 'https://bearings-info.herokuapp.com/'
+        publicPath: process.env.NODE_ENV === 'production'
+            ? '/vue-page-test/'
+            : '/'
     }
 }
